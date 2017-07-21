c95eed275ad7c8f0a76032f592be9995bbc41a01
无昂KKK <414455322@qq.com>
2017-07-21 10:04:38 +0800

    Initial commit

---
+++ src/main/java/com/wyl/wenda/model/User.java
@@ -0,0 +1,63 @@
+package com.wyl.wenda.model;
+
+import java.util.Date;
+/*
+* 用户类
+* */
+public class User {
+    private int id;//用户ID
+    private String username;//用户账号
+    private String password;//用户密码
+    private String salt;//给密码的杂质，保证安全性
+    private String nickname;//昵称
+    private Date update;//用户创建的时间
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Date update) {
+        this.update = update;
+    }
+}
